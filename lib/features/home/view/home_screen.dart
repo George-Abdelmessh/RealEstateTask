a78b023654d27eb25fe8a1000d278bb50a3bb5b4
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2023-11-04 01:42:16 +0200

    final task 2

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:real_estate_task/core/constant/screen_size.dart';
 import 'package:real_estate_task/core/style/app_color.dart';
 import 'package:real_estate_task/features/home/view/cards/app_bar_card.dart';
+import 'package:real_estate_task/features/home/view/cards/bottom_nav_bar.dart';
 import 'package:real_estate_task/features/home/view/widgets/info_widget.dart';
 import 'package:real_estate_task/features/home/view/widgets/service_widget.dart';
 import 'package:real_estate_task/features/home/view/widgets/slider_widget.dart';
@@ -14,28 +15,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarDesign(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const[
-           BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
-          ),
-           BottomNavigationBarItem(
-            icon: Icon(Icons.calculate_outlined),
-            label: 'Asset',
-          ),
-           BottomNavigationBarItem(
-            icon: Icon(Icons.support_agent_outlined),
-            label: 'Support',
-          ),
-           BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: 0,
-        // backgroundColor: ,
-      ),
+      bottomNavigationBar: bottomNavigationBarDesign(),
       backgroundColor: AppColor.lightGrey,
       body: SingleChildScrollView(
         child: Column(
@@ -47,14 +27,17 @@ class HomeScreen extends StatelessWidget {
               ),
               child: Column(
                 children: [
+                  /// User Welcome Card
                   const UserCard(),
                   SizedBox(
                     height: AppSize.heightScale(context, 16),
                   ),
+                  /// Main Screen Info Widget
                   const InfoWidget(),
                   SizedBox(
                     height: AppSize.heightScale(context, 10),
                   ),
+                  /// Slider Widget With Indicator
                   const SliderWidget(),
                 ],
               ),
@@ -62,6 +45,7 @@ class HomeScreen extends StatelessWidget {
             SizedBox(
               height: AppSize.heightScale(context, 3),
             ),
+            /// Service Widget With 3 Views
             const ServiceWidget(),
           ],
         ),
